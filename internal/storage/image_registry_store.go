715bd0560df20b755d9cb42a83ca8c0d8f8b283e
Fabrizio Sestito <fabrizio.sestito@suse.com>
2025-11-21 13:35:38 +0100

    feat(storage): instantiate nats watcher and return registry store wrapper struct
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- internal/storage/image_registry_store.go
+++ internal/storage/image_registry_store.go
@@ -7,11 +7,17 @@ import (
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/kubewarden/sbomscanner/api/storage/v1alpha1"
+	"github.com/nats-io/nats.go"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/apiserver/pkg/registry/generic"
 	"k8s.io/apiserver/pkg/registry/generic/registry"
+)
+
+const (
+	imageResourceSingularName = "image"
+	imageResourcePluralName   = "images"
 )
 
 const CreateImageTableSQL = `
@@ -28,28 +34,35 @@ func NewImageStore(
 	scheme *runtime.Scheme,
 	optsGetter generic.RESTOptionsGetter,
 	db *pgxpool.Pool,
+	nc *nats.Conn,
 	logger *slog.Logger,
-) (*registry.Store, error) {
+) (*RegistryStoreWithWatcher, error) {
 	strategy := newImageStrategy(scheme)
-
 	newFunc := func() runtime.Object { return &v1alpha1.Image{} }
 	newListFunc := func() runtime.Object { return &v1alpha1.ImageList{} }
 
-	store := &registry.Store{
+	watchBroadcaster := watch.NewBroadcaster(1000, watch.WaitIfChannelFull)
+	natsBroadcaster := newNatsBroadcaster(nc, imageResourcePluralName, watchBroadcaster, logger)
+
+	store := &store{
+		db:          db,
+		broadcaster: natsBroadcaster,
+		table:       imageResourcePluralName,
+		newFunc:     newFunc,
+		newListFunc: newListFunc,
+		logger:      logger.With("store", imageResourceSingularName),
+	}
+
+	natsWatcher := newNatsWatcher(nc, imageResourcePluralName, watchBroadcaster, store, logger)
+
+	registryStore := &registry.Store{
 		NewFunc:                   newFunc,
 		NewListFunc:               newListFunc,
 		PredicateFunc:             matcher,
-		DefaultQualifiedResource:  v1alpha1.Resource("images"),
-		SingularQualifiedResource: v1alpha1.Resource("image"),
+		DefaultQualifiedResource:  v1alpha1.Resource(imageResourcePluralName),
+		SingularQualifiedResource: v1alpha1.Resource(imageResourceSingularName),
 		Storage: registry.DryRunnableStorage{
-			Storage: &store{
-				db:          db,
-				broadcaster: watch.NewBroadcaster(1000, watch.WaitIfChannelFull),
-				table:       "images",
-				newFunc:     newFunc,
-				newListFunc: newListFunc,
-				logger:      logger.With("store", "image"),
-			},
+			Storage: store,
 		},
 		CreateStrategy: strategy,
 		UpdateStrategy: strategy,
@@ -58,11 +71,14 @@ func NewImageStore(
 	}
 
 	options := &generic.StoreOptions{RESTOptions: optsGetter, AttrFunc: getAttrs}
-	if err := store.CompleteWithOptions(options); err != nil {
+	if err := registryStore.CompleteWithOptions(options); err != nil {
 		return nil, fmt.Errorf("unable to complete store with options: %w", err)
 	}
 
-	return store, nil
+	return &RegistryStoreWithWatcher{
+		store:   registryStore,
+		watcher: natsWatcher,
+	}, nil
 }
 
 type imageTableConvertor struct{}
