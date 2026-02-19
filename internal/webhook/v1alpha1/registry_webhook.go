kubewarden/sbomscanner

feat(controller): prevent user changes to managed Registries
Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

internal/webhook/v1alpha1/registry_webhook.go
@@ -2,6 +2,7 @@ package v1alpha1
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/go-logr/logr"
@@ -11,6 +12,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
+	"github.com/kubewarden/sbomscanner/api"
 	"github.com/kubewarden/sbomscanner/api/v1alpha1"
 )
 
@@ -21,10 +23,12 @@ const (
 var availableCatalogTypes = []string{v1alpha1.CatalogTypeNoCatalog, v1alpha1.CatalogTypeOCIDistribution}
 
 // SetupRegistryWebhookWithManager registers the webhook for Registry in the manager.
-func SetupRegistryWebhookWithManager(mgr ctrl.Manager) error {
+func SetupRegistryWebhookWithManager(mgr ctrl.Manager, serviceAccountNamespace, serviceAccountName string) error {
 	err := ctrl.NewWebhookManagedBy(mgr, &v1alpha1.Registry{}).
 		WithValidator(&RegistryCustomValidator{
-			logger: mgr.GetLogger().WithName("registry_validator"),
+			serviceAccountNamespace: serviceAccountNamespace,
+			serviceAccountName:      serviceAccountName,
+			logger:                  mgr.GetLogger().WithName("registry_validator"),
 		}).
 		WithDefaulter(&RegistryCustomDefaulter{
 			logger: mgr.GetLogger().WithName("registry_defaulter"),
@@ -55,17 +59,23 @@ func (d *RegistryCustomDefaulter) Default(_ context.Context, registry *v1alpha1.
 	return nil
 }
 
-// +kubebuilder:webhook:path=/validate-sbomscanner-kubewarden-io-v1alpha1-registry,mutating=false,failurePolicy=fail,sideEffects=None,groups=sbomscanner.kubewarden.io,resources=registries,verbs=create;update,versions=v1alpha1,name=vregistry.sbomscanner.kubewarden.io,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-sbomscanner-kubewarden-io-v1alpha1-registry,mutating=false,failurePolicy=fail,sideEffects=None,groups=sbomscanner.kubewarden.io,resources=registries,verbs=create;update;delete,versions=v1alpha1,name=vregistry.sbomscanner.kubewarden.io,admissionReviewVersions=v1
 
 type RegistryCustomValidator struct {
-	logger logr.Logger
+	serviceAccountNamespace string
+	serviceAccountName      string
+	logger                  logr.Logger
 }
 
 var _ admission.Validator[*v1alpha1.Registry] = &RegistryCustomValidator{}
 
 // ValidateCreate implements admission.Validator so a webhook will be registered for the type Registry.
-func (v *RegistryCustomValidator) ValidateCreate(_ context.Context, registry *v1alpha1.Registry) (admission.Warnings, error) {
+func (v *RegistryCustomValidator) ValidateCreate(ctx context.Context, registry *v1alpha1.Registry) (admission.Warnings, error) {
 	v.logger.Info("Validation for Registry upon creation", "name", registry.GetName())
+
+	if err := v.validatedManagedRegistry(ctx, registry); err != nil {
+		return nil, err
+	}
 
 	allErrs := validateRegistry(registry)
 
@@ -81,8 +91,12 @@ func (v *RegistryCustomValidator) ValidateCreate(_ context.Context, registry *v1
 }
 
 // ValidateUpdate implements admission.Validator so a webhook will be registered for the type Registry.
-func (v *RegistryCustomValidator) ValidateUpdate(_ context.Context, _, registry *v1alpha1.Registry) (admission.Warnings, error) {
+func (v *RegistryCustomValidator) ValidateUpdate(ctx context.Context, _, registry *v1alpha1.Registry) (admission.Warnings, error) {
 	v.logger.Info("Validation for Registry upon update", "name", registry.GetName())
+
+	if err := v.validatedManagedRegistry(ctx, registry); err != nil {
+		return nil, err
+	}
 
 	allErrs := validateRegistry(registry)
 
@@ -98,10 +112,38 @@ func (v *RegistryCustomValidator) ValidateUpdate(_ context.Context, _, registry 
 }
 
 // ValidateDelete implements admission.Validator so a webhook will be registered for the type Registry.
-func (v *RegistryCustomValidator) ValidateDelete(_ context.Context, registry *v1alpha1.Registry) (admission.Warnings, error) {
+func (v *RegistryCustomValidator) ValidateDelete(ctx context.Context, registry *v1alpha1.Registry) (admission.Warnings, error) {
 	v.logger.Info("Validation for Registry upon deletion", "name", registry.GetName())
 
+	if err := v.validatedManagedRegistry(ctx, registry); err != nil {
+		return nil, err
+	}
+
 	return nil, nil
+}
+
+// validatedManagedRegistry ensures that only the designated service account can modify managed Registry resources.
+func (v *RegistryCustomValidator) validatedManagedRegistry(ctx context.Context, registry *v1alpha1.Registry) error {
+	managedLabel := registry.GetLabels()[api.LabelManagedByKey]
+	if managedLabel != api.LabelManagedByValue {
+		return nil
+	}
+
+	request, err := admission.RequestFromContext(ctx)
+	if err != nil {
+		return apierrors.NewInternalError(errors.New("unable to retrieve admission request from context"))
+	}
+
+	allowedUsername := fmt.Sprintf("system:serviceaccount:%s:%s", v.serviceAccountNamespace, v.serviceAccountName)
+	if request.UserInfo.Username != allowedUsername {
+		return apierrors.NewForbidden(
+			v1alpha1.GroupVersion.WithResource("registries").GroupResource(),
+			registry.Name,
+			errors.New("only the designated service account can modify managed Registry resources"),
+		)
+	}
+
+	return nil
 }
 
 func validateRegistry(registry *v1alpha1.Registry) field.ErrorList {

fabriziosestito/sbombastic

internal/webhook/v1alpha1/registry_webhook.go
@@ -2,6 +2,7 @@ package v1alpha1
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/go-logr/logr"
@@ -11,6 +12,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
+	"github.com/kubewarden/sbomscanner/api"
 	"github.com/kubewarden/sbomscanner/api/v1alpha1"
 )
 
@@ -21,10 +23,12 @@ const (
 var availableCatalogTypes = []string{v1alpha1.CatalogTypeNoCatalog, v1alpha1.CatalogTypeOCIDistribution}
 
 // SetupRegistryWebhookWithManager registers the webhook for Registry in the manager.
-func SetupRegistryWebhookWithManager(mgr ctrl.Manager) error {
+func SetupRegistryWebhookWithManager(mgr ctrl.Manager, serviceAccountNamespace, serviceAccountName string) error {
 	err := ctrl.NewWebhookManagedBy(mgr, &v1alpha1.Registry{}).
 		WithValidator(&RegistryCustomValidator{
-			logger: mgr.GetLogger().WithName("registry_validator"),
+			serviceAccountNamespace: serviceAccountNamespace,
+			serviceAccountName:      serviceAccountName,
+			logger:                  mgr.GetLogger().WithName("registry_validator"),
 		}).
 		WithDefaulter(&RegistryCustomDefaulter{
 			logger: mgr.GetLogger().WithName("registry_defaulter"),
@@ -55,17 +59,23 @@ func (d *RegistryCustomDefaulter) Default(_ context.Context, registry *v1alpha1.
 	return nil
 }
 
-// +kubebuilder:webhook:path=/validate-sbomscanner-kubewarden-io-v1alpha1-registry,mutating=false,failurePolicy=fail,sideEffects=None,groups=sbomscanner.kubewarden.io,resources=registries,verbs=create;update,versions=v1alpha1,name=vregistry.sbomscanner.kubewarden.io,admissionReviewVersions=v1
+// +kubebuilder:webhook:path=/validate-sbomscanner-kubewarden-io-v1alpha1-registry,mutating=false,failurePolicy=fail,sideEffects=None,groups=sbomscanner.kubewarden.io,resources=registries,verbs=create;update;delete,versions=v1alpha1,name=vregistry.sbomscanner.kubewarden.io,admissionReviewVersions=v1
 
 type RegistryCustomValidator struct {
-	logger logr.Logger
+	serviceAccountNamespace string
+	serviceAccountName      string
+	logger                  logr.Logger
 }
 
 var _ admission.Validator[*v1alpha1.Registry] = &RegistryCustomValidator{}
 
 // ValidateCreate implements admission.Validator so a webhook will be registered for the type Registry.
-func (v *RegistryCustomValidator) ValidateCreate(_ context.Context, registry *v1alpha1.Registry) (admission.Warnings, error) {
+func (v *RegistryCustomValidator) ValidateCreate(ctx context.Context, registry *v1alpha1.Registry) (admission.Warnings, error) {
 	v.logger.Info("Validation for Registry upon creation", "name", registry.GetName())
+
+	if err := v.validatedManagedRegistry(ctx, registry); err != nil {
+		return nil, err
+	}
 
 	allErrs := validateRegistry(registry)
 
@@ -81,8 +91,12 @@ func (v *RegistryCustomValidator) ValidateCreate(_ context.Context, registry *v1
 }
 
 // ValidateUpdate implements admission.Validator so a webhook will be registered for the type Registry.
-func (v *RegistryCustomValidator) ValidateUpdate(_ context.Context, _, registry *v1alpha1.Registry) (admission.Warnings, error) {
+func (v *RegistryCustomValidator) ValidateUpdate(ctx context.Context, oldRegistry, registry *v1alpha1.Registry) (admission.Warnings, error) {
 	v.logger.Info("Validation for Registry upon update", "name", registry.GetName())
+
+	if err := v.validatedManagedRegistry(ctx, oldRegistry); err != nil {
+		return nil, err
+	}
 
 	allErrs := validateRegistry(registry)
 
@@ -98,10 +112,38 @@ func (v *RegistryCustomValidator) ValidateUpdate(_ context.Context, _, registry 
 }
 
 // ValidateDelete implements admission.Validator so a webhook will be registered for the type Registry.
-func (v *RegistryCustomValidator) ValidateDelete(_ context.Context, registry *v1alpha1.Registry) (admission.Warnings, error) {
+func (v *RegistryCustomValidator) ValidateDelete(ctx context.Context, registry *v1alpha1.Registry) (admission.Warnings, error) {
 	v.logger.Info("Validation for Registry upon deletion", "name", registry.GetName())
 
+	if err := v.validatedManagedRegistry(ctx, registry); err != nil {
+		return nil, err
+	}
+
 	return nil, nil
+}
+
+// validatedManagedRegistry ensures that only the designated service account can modify managed Registry resources.
+func (v *RegistryCustomValidator) validatedManagedRegistry(ctx context.Context, registry *v1alpha1.Registry) error {
+	managedLabel := registry.GetLabels()[api.LabelManagedByKey]
+	if managedLabel != api.LabelManagedByValue {
+		return nil
+	}
+
+	request, err := admission.RequestFromContext(ctx)
+	if err != nil {
+		return apierrors.NewInternalError(errors.New("unable to retrieve admission request from context"))
+	}
+
+	allowedUsername := fmt.Sprintf("system:serviceaccount:%s:%s", v.serviceAccountNamespace, v.serviceAccountName)
+	if request.UserInfo.Username != allowedUsername {
+		return apierrors.NewForbidden(
+			v1alpha1.GroupVersion.WithResource("registries").GroupResource(),
+			registry.Name,
+			errors.New("only the designated service account can modify managed Registry resources"),
+		)
+	}
+
+	return nil
 }
 
 func validateRegistry(registry *v1alpha1.Registry) field.ErrorList {